7010e06d3fbe47c20dbc573ee5234d87d673a158
genie97 <qndnd1@naver.com>
2020-07-06 15:45:58 +0900

    first reactive app (facebook lab tutorial)

---
+++ Web/React Native/React Native App/ReactNative-Coin-Tutorial/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
+import CoinView from './screens/CoinView';
+import TopBar from './components/TopBar';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshDate: '-',
+    }
+  }
+
+  _setRefreshDate = (date) => { // Called from CoinView's prop
+    console.log('Updated: '+ date);
+    this.setState({
+      refreshDate: date,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.statusBar} />
+        <TopBar title="Show Me The Coin" refreshDate={this.state.refreshDate} />
+        <CoinView
+          refreshDate={this._setRefreshDate}
+          style={styles.coinView}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  statusBar: {
+    backgroundColor: '#C2185B',
+    height: Constants.statusBarHeight
+  },
+  container: {
+    flex: 1
+  },
+  coinView: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column', // row
+    backgroundColor: 'white',
+    //alignItems: 'center',
+    //justifyContent: 'flex-start' // center, space-around
+  }
+});
